63b9fc7d74af6ef2542c8b1899d0aa68078ca187
Ottoniel R <jorv15spawn@gmail.com>
2019-08-04 17:18:54 -0600

    Added application properties

--- src/main/java/com/mobiquityinc/PackageApplication.java
+++ src/main/java/com/mobiquityinc/PackageApplication.java
@@ -2,11 +2,21 @@ package com.mobiquityinc;
 
 import com.mobiquityinc.packer.Packer;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class PackageApplication {
 
     public static void main(String[] args){
         try {
-            Packer.pack("C:\\Users\\Puma\\Documents\\Mobiquity\\correct.txt");
+            InputStream input = PackageApplication.class.getClassLoader().getResourceAsStream("application.properties");
+            final Properties properties = new Properties();
+            if (input == null) {
+                System.out.println("Sorry, unable to find application properties");
+                return;
+            }
+            properties.load(input);
+            System.out.println(Packer.pack(properties.getProperty("input.file.path")));
         }catch (Exception e){
             e.printStackTrace();
         }
--- src/main/java/com/mobiquityinc/packer/Packer.java
+++ src/main/java/com/mobiquityinc/packer/Packer.java
@@ -7,10 +7,12 @@ import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -18,20 +20,24 @@ import java.util.stream.Collectors;
 public class Packer {
 
     private final static Logger LOGGER = Logger.getLogger(Packer.class);
+    private final static Properties PROPERTIES = getProperties();
 
     private Packer() {
     }
 
     public static String pack(final String filePath) throws APIException {
+        final int maxItems = Integer.parseInt(PROPERTIES.getProperty("items.max"));
+        final int maxCapacity = Integer.parseInt(PROPERTIES.getProperty("capacity.max")) * 100;
+
         final List<String> lines = getDataFromFile(filePath);
         if (lines != null) {
-            if (lines.size() > 15) {
-                throw new APIException("Items exceed maximum capacity of 15");
+            if (lines.size() > maxItems) {
+                throw new APIException("Items exceed maximum capacity of " + maxItems);
             }
             final StringBuilder stringBuilder = new StringBuilder();
             for (final String line : lines) {
                 final int packageCapacity = getPackageCapacity(line);
-                if (packageCapacity < 0 || packageCapacity > 10000) {
+                if (packageCapacity < 0 || packageCapacity > maxCapacity) {
                     throw new APIException("Invalid package capacity found");
                 }
                 final List<Item> items = getItemsFromData(line);
@@ -49,52 +55,67 @@ public class Packer {
         //Sort by weight so Dynamic programming returns the values with less weight in case of combinations with same price
         items.sort(new ItemComparator());
         final StringBuilder stringBuilder = new StringBuilder();
-       final int[][] matrix = new int[items.size() + 1][capacity + 1];
+        final int[][] matrix = new int[items.size() + 1][capacity + 1];
 
         for (int i = 0; i <= capacity; i++) {
             matrix[0][i] = 0;
         }
 
         for (int i = 1; i <= items.size(); i++) {
-            for(int j = 0; j<= capacity; j++ ){
-                final Item previousItem = items.get(i-1);
+            for (int j = 0; j <= capacity; j++) {
+                final Item previousItem = items.get(i - 1);
                 final int previousWeight = previousItem.getWeight();
                 final int previousValue = previousItem.getValue();
-                if(previousWeight > j){
-                    matrix[i][j] = matrix[i-1][j];
-                }
-                else{
-                    matrix[i][j] = Math.max(matrix[i-1][j], matrix[i-1][j-previousWeight]+ previousValue);
+                if (previousWeight > j) {
+                    matrix[i][j] = matrix[i - 1][j];
+                } else {
+                    matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i - 1][j - previousWeight] + previousValue);
                 }
             }
         }
 
+        //Once we have calculated our DP matrix we need to get the items for output.
         int total = matrix[items.size()][capacity];
         int totalWeight = capacity;
-        for (int i = items.size(); i>0 && total > 0; i--){
-            if(total != matrix[i-1][totalWeight]){
-                final Item previousItem = items.get(i-1);
+        for (int i = items.size(); i > 0 && total > 0; i--) {
+            if (total != matrix[i - 1][totalWeight]) {
+                final Item previousItem = items.get(i - 1);
                 total -= previousItem.getValue();
                 totalWeight -= previousItem.getWeight();
-                if(stringBuilder.length()==0){
+                if (stringBuilder.length() == 0) {
                     stringBuilder.append(previousItem.getId());
-                }
-                else {
+                } else {
                     stringBuilder.append(", ");
                     stringBuilder.append(previousItem.getId());
                 }
             }
         }
-        if(stringBuilder.length()==0){
+        if (stringBuilder.length() == 0) {
             return "-";
         }
         return stringBuilder.toString();
     }
 
+    private static Properties getProperties() {
+        try {
+            final InputStream input = Packer.class.getClassLoader().getResourceAsStream("application.properties");
+            final Properties properties = new Properties();
+            if (input == null) {
+                LOGGER.error("Unable to find application properties");
+                return null;
+            }
+            properties.load(input);
+            return properties;
+        } catch (IOException ioException) {
+            LOGGER.error("Error reading Application properties");
+        }
+        return null;
+    }
+
     private static int getPackageCapacity(final String data) {
         try {
             final String rawNumber = data.substring(0, data.lastIndexOf(':'));
-            return Integer.parseInt(rawNumber.trim())*100;
+            return Integer.parseInt(rawNumber.trim()) * 100;
         } catch (NumberFormatException numberFormatExpeption) {
             LOGGER.error("Exception while trying to parse package capacity: ", numberFormatExpeption);
         }
@@ -125,9 +146,13 @@ public class Packer {
                 final int id = Integer.parseInt(itemProperties[0].trim());
                 //Had to convert weights to integer in order to use dynamic programming.
                 //weights with more than 2 decimal places will be rounded.
-                final int weight = (int) Double.parseDouble(itemProperties[1].trim())*100;
+                final int weight = (int) Double.parseDouble(itemProperties[1].trim()) * 100;
                 final int value = Integer.parseInt(itemProperties[2].substring(1).trim());
-                if (weight > 10000 || value > 100) {
+
+                final int maxWeight = Integer.parseInt(PROPERTIES.getProperty("weight.max")) * 100;
+                final int maxValue = Integer.parseInt(PROPERTIES.getProperty("value.max"));
+
+                if (weight > maxWeight || value > maxValue) {
                     throw new APIException("weight or cost exceed permitted values");
                 }
                 final Item item = new Item(id, weight, value);
